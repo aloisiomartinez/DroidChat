a43777a58530c79429388a4588c79003e5fb0481
aloisiomartinez <aloisiojacobs36@gmail.com>
2025-01-23 15:03:39 -0300

    SafeArgs for Compose

--- app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
+++ app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
@@ -2,28 +2,39 @@ package com.example.droidchat.navigation
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.droidchat.ui.feature.splash.SplashRoute
+import kotlinx.coroutines.delay
+import kotlinx.serialization.Serializable
+import kotlin.math.sign
 
-const val SPLASH_ROUTE = "splash"
-const val SIGN_IN_ROUTE = "signIn"
-const val SIGN_UP_ROUTE = "signUp"
+@Serializable
+object SplashRoute
+
+@Serializable
+object SignInRoute
+
+@Serializable
+object SignUpRoute
 
 @Composable
 fun ChatNavHost() {
     val navController = rememberNavController()
     
-    NavHost(navController = navController, startDestination = SPLASH_ROUTE) {
-        composable(SPLASH_ROUTE) {
+    NavHost(navController = navController, startDestination = SplashRoute) {
+        composable<SplashRoute> {
             SplashRoute()
-        }
-        composable(SIGN_IN_ROUTE) {
 
         }
-        composable(SIGN_UP_ROUTE) {}
+        composable<SignInRoute>() {
+
+        }
+        composable<SignUpRoute> {}
     }
 }
