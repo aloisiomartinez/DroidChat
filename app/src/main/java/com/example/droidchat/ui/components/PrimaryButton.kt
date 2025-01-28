6c1b487252b1833ed2793dd7386ce0b65d3f8ec1
aloisiomartinez <aloisiojacobs36@gmail.com>
2025-01-28 17:55:37 -0300

    PrimaryButton and SignInScreen finish

---
+++ app/src/main/java/com/example/droidchat/ui/components/PrimaryButton.kt
@@ -0,0 +1,86 @@
+package com.example.droidchat.ui.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.droidchat.ui.theme.DroidChatTheme
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false
+) {
+
+
+    Button(
+        onClick = { onClick() },
+        modifier = modifier
+            .height (64.dp),
+        enabled = !isLoading,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContentColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .animateContentSize()
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(30.dp)
+                        .aspectRatio(1f),
+                    strokeCap = StrokeCap.Round,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            } else {
+                Text(
+                    text = text,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PrimaryButtonPreview() {
+    DroidChatTheme {
+        PrimaryButton(
+            text = "SignIn",
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PrimaryButtonLoadingPreview() {
+    DroidChatTheme {
+        PrimaryButton(
+            text = "SignIn",
+            isLoading = true,
+            onClick = {}
+        )
+    }
+}
--- app/src/main/java/com/example/droidchat/ui/feature/signin/SignInScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/signin/SignInScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -25,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
 import com.example.droidchat.navigation.SignInRoute
+import com.example.droidchat.ui.components.PrimaryButton
 import com.example.droidchat.ui.components.PrimaryTextField
 import com.example.droidchat.ui.theme.BackgroundGradient
 import com.example.droidchat.ui.theme.DroidChatTheme
@@ -38,10 +42,10 @@ fun SignInRoute() {
 fun SignInScreen() {
 
 
-
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(brush = BackgroundGradient),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -52,12 +56,11 @@ fun SignInScreen() {
             contentDescription = null
         )
 
-        Spacer(modifier = Modifier.height(64.dp))
+        Spacer(modifier = Modifier.height(78.dp))
 
         var email by remember {
             mutableStateOf("")
         }
-
 
         PrimaryTextField(
             value = email,
@@ -65,14 +68,13 @@ fun SignInScreen() {
                 email = it
             },
             modifier = Modifier
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = dimensionResource(id = R.dimen.spacing_medium)),
             placeholder = stringResource(id = R.string.feature_login_email),
             leadingIcon = R.drawable.ic_envelope,
             keyboardType = KeyboardType.Email
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
+        Spacer(modifier = Modifier.height(14.dp))
 
         var password by remember {
             mutableStateOf("")
@@ -84,12 +86,29 @@ fun SignInScreen() {
                 password = it
             },
             modifier = Modifier
-                .padding(horizontal = 16.dp),
+                .padding(horizontal = dimensionResource(id = R.dimen.spacing_medium)),
             placeholder = stringResource(id = R.string.feature_login_password),
             leadingIcon = R.drawable.ic_lock,
             keyboardType = KeyboardType.Password,
             imeAction = ImeAction.Done
         )
+
+        Spacer(modifier = Modifier.height(98.dp))
+
+        var isLoading by remember {
+            mutableStateOf(false)
+        }
+
+        PrimaryButton(
+            text = stringResource(id = R.string.feature_login_button),
+            onClick = {
+                isLoading = !isLoading
+            },
+            modifier = Modifier
+                .padding(horizontal = dimensionResource(id = R.dimen.spacing_medium)),
+            isLoading = isLoading
+        )
+
     }
 }
 
