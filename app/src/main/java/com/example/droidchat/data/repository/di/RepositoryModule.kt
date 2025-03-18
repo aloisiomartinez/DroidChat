0b1d93ef279ed23c91923d5680349ee195c7d613
aloisiomartinez <aloisiojacobs36@gmail.com>
2025-03-18 07:16:11 -0300

    authRepository

---
+++ app/src/main/java/com/example/droidchat/data/repository/di/RepositoryModule.kt
@@ -0,0 +1,15 @@
+package com.example.droidchat.data.repository.di
+
+import com.example.droidchat.data.repository.AuthRepository
+import com.example.droidchat.data.repository.AuthRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+interface RepositoryModule {
+    @Binds
+    fun bindAuthRepository(repositoryAuthRepositoryImpl: AuthRepositoryImpl): AuthRepository
+}
